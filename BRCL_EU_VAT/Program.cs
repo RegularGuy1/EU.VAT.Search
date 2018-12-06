5a0b009e495ad25bbfeb02c7cf1d9c7649797160
Vadim Skvirsky <v.skvirsky@cleo.one>
2018-12-06 09:48:23 +0100

    Add project files.

---
+++ BRCL_EU_VAT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BRCL_EU_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
